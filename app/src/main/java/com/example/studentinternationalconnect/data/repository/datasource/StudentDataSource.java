aa24afb83e70c3ce99547590507445b35fdae98e
Maniharika-1 <maniharika1998@gmail.com>
2023-02-23 18:18:08 +0530

    Added StudentDataSource interface and implementation class.

---
+++ app/src/main/java/com/example/studentinternationalconnect/data/repository/datasource/StudentDataSource.java
@@ -0,0 +1,9 @@
+package com.example.studentinternationalconnect.data.repository.datasource;
+
+import com.example.studentinternationalconnect.data.model.Student;
+
+public interface StudentDataSource {
+
+    void saveStudentInfo(Student student);
+
+}
---
+++ app/src/main/java/com/example/studentinternationalconnect/data/repository/datasourceImpl/StudentDataSourceImpl.java
@@ -0,0 +1,13 @@
+package com.example.studentinternationalconnect.data.repository.datasourceImpl;
+
+import com.example.studentinternationalconnect.data.model.Student;
+import com.example.studentinternationalconnect.data.repository.datasource.StudentDataSource;
+
+public class StudentDataSourceImpl implements StudentDataSource {
+
+    @Override
+    public void saveStudentInfo(Student student) {
+
+    }
+
+}
