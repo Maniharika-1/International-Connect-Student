afc9b1b6416cd9269ca014335a08dbb2f356b284
Maniharika-1 <maniharika1998@gmail.com>
2023-03-06 20:18:02 +0530

    Updated saveStudentInfo() in StudentRepositoryImpl

--- app/src/main/java/com/example/studentinternationalconnect/domain/repository/StudentRepositoryImpl.java
+++ app/src/main/java/com/example/studentinternationalconnect/domain/repository/StudentRepositoryImpl.java
@@ -2,6 +2,9 @@ package com.example.studentinternationalconnect.domain.repository;
 
 import com.example.studentinternationalconnect.data.model.Student;
 import com.example.studentinternationalconnect.data.repository.datasource.StudentDataSource;
+import com.example.studentinternationalconnect.domain.util.GetDataListener;
+import com.example.studentinternationalconnect.domain.util.OnCompleteListener;
+import com.google.firebase.database.DatabaseReference;
 
 public class StudentRepositoryImpl implements StudentRepository{
 
@@ -12,7 +15,8 @@ public class StudentRepositoryImpl implements StudentRepository{
     }
 
     @Override
-    public void saveStudentInfo(Student student) {
-        studentDataSource.saveStudentInfo(student);
+    public void saveStudentInfo(Student student, DatabaseReference studentNode, OnCompleteListener onCompleteListener) {
+        studentDataSource.saveStudentInfo(student, studentNode, onCompleteListener);
     }
+
 }
