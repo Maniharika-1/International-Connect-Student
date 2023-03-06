e093c9141d95205b1f0eaf02a82dc88dfc68d814
Maniharika-1 <maniharika1998@gmail.com>
2023-03-06 20:15:08 +0530

    Added implementation for saveStudentInfo() in StudentDataSourceImpl.

--- app/src/main/java/com/example/studentinternationalconnect/data/repository/datasourceImpl/StudentDataSourceImpl.java
+++ app/src/main/java/com/example/studentinternationalconnect/data/repository/datasourceImpl/StudentDataSourceImpl.java
@@ -1,13 +1,33 @@
 package com.example.studentinternationalconnect.data.repository.datasourceImpl;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.studentinternationalconnect.data.model.Student;
 import com.example.studentinternationalconnect.data.repository.datasource.StudentDataSource;
+import com.example.studentinternationalconnect.domain.util.GetDataListener;
+import com.example.studentinternationalconnect.domain.util.OnCompleteListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class StudentDataSourceImpl implements StudentDataSource {
 
     @Override
-    public void saveStudentInfo(Student student) {
+    public void saveStudentInfo(Student student, DatabaseReference studentNode, OnCompleteListener onCompleteListener) {
 
+        onCompleteListener.onStart();
+        String studentKey = studentNode.push().getKey();
+        studentNode.child(studentKey).setValue(student).addOnCompleteListener(task -> {
+
+            if (task.isSuccessful())
+                onCompleteListener.onSuccess();
+            else onCompleteListener.onFailure();
+
+        });
     }
+
 
 }
