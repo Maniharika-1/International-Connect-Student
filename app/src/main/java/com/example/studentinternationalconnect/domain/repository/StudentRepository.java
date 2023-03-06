7275f4cebdb6ab23d8b6604af6248426064a059a
Maniharika-1 <maniharika1998@gmail.com>
2023-03-06 20:16:28 +0530

    Updated saveStudentInfo() method signature.

--- app/src/main/java/com/example/studentinternationalconnect/domain/repository/StudentRepository.java
+++ app/src/main/java/com/example/studentinternationalconnect/domain/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.example.studentinternationalconnect.domain.repository;
 
 import com.example.studentinternationalconnect.data.model.Student;
+import com.example.studentinternationalconnect.domain.util.GetDataListener;
+import com.example.studentinternationalconnect.domain.util.OnCompleteListener;
+import com.google.firebase.database.DatabaseReference;
 
 public interface StudentRepository {
 
-    void saveStudentInfo(Student student);
+    void saveStudentInfo(Student student, DatabaseReference studentNode, OnCompleteListener onCompleteListener);
 
 }
