67012d362e72181c62d0549b573031d302b54580
Maniharika-1 <maniharika1998@gmail.com>
2023-03-06 20:19:38 +0530

    Updated execute() in SignUpUseCase

--- app/src/main/java/com/example/studentinternationalconnect/domain/usecase/SignUpUseCase.java
+++ app/src/main/java/com/example/studentinternationalconnect/domain/usecase/SignUpUseCase.java
@@ -2,6 +2,8 @@ package com.example.studentinternationalconnect.domain.usecase;
 
 import com.example.studentinternationalconnect.data.model.Student;
 import com.example.studentinternationalconnect.domain.repository.StudentRepository;
+import com.example.studentinternationalconnect.domain.util.OnCompleteListener;
+import com.google.firebase.database.DatabaseReference;
 
 public class SignUpUseCase {
 
@@ -10,7 +12,7 @@ public class SignUpUseCase {
         this.studentRepository = studentRepository;
     }
 
-    public void execute(Student student) {
-        studentRepository.saveStudentInfo(student);
+    public void execute(Student student, DatabaseReference studentNode, OnCompleteListener onCompleteListener) {
+        studentRepository.saveStudentInfo(student, studentNode, onCompleteListener);
     }
 }
