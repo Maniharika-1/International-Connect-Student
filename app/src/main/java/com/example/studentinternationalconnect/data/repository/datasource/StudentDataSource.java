212a290fb525474499cbda69208c6772f3c532d0
Maniharika-1 <maniharika1998@gmail.com>
2023-03-06 20:10:25 +0530

    Added database reference and OnCompleteListener interface to saveStudentInfo() method signature.

--- app/src/main/java/com/example/studentinternationalconnect/data/repository/datasource/StudentDataSource.java
+++ app/src/main/java/com/example/studentinternationalconnect/data/repository/datasource/StudentDataSource.java
@@ -1,9 +1,11 @@
 package com.example.studentinternationalconnect.data.repository.datasource;
 
 import com.example.studentinternationalconnect.data.model.Student;
+import com.example.studentinternationalconnect.domain.util.OnCompleteListener;
+import com.google.firebase.database.DatabaseReference;
 
 public interface StudentDataSource {
 
-    void saveStudentInfo(Student student);
+    void saveStudentInfo(Student student, DatabaseReference studentNode, OnCompleteListener onCompleteListener);
 
 }
